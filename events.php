5a57acd7a1b7df2057e322c00966e37ff981b019
HPIE <sunil@hpie.in>
2020-12-12 12:46:25 +0530

    Added events page

---
+++ events.php
@@ -0,0 +1,334 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Head Section Start -->
+<?php
+	include ("_partials/header.php");
+?>
+<!-- Head Section End -->
+
+  <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+
+    
+    <div class="site-wrap" id="home-section">
+
+      <div class="site-mobile-menu site-navbar-target">
+        <div class="site-mobile-menu-header">
+          <div class="site-mobile-menu-close mt-3">
+            <span class="icon-close2 js-menu-toggle"></span>
+          </div>
+        </div>
+        <div class="site-mobile-menu-body"></div>
+      </div>
+
+
+<!-- Menu Section Start -->      
+<?php
+	include ("_partials/menu.php");
+?>
+<!-- Menu Section End -->
+
+
+
+    <div class="owl-carousel-wrapper">
+
+      
+
+      <div class="box-92819">
+        <h1 class="text-white mb-3">Events</h1>
+        <p class="lead text-white">The Commission organizes various events within the state and at national level to build public awareness and create a moral force in the country to stand by children and protect their rights.</p>
+        <p class="lead text-white"><a id="readmorelnk" href="#readmorediv">... read more</a></p>
+      </div>
+
+      
+        <div class="ftco-cover-1 overlay" style="background-image: url('images/hpcpcr-school-fare-strips.jpg');"></div>
+      
+    </div>
+    
+    
+    <div class="site-section">
+      <div class="container">
+        
+        <div class="row mb-12 align-items-st">
+          <div class="col-md-12" id="readmorediv" style="display: none;">
+            <div class="heading-20219">
+              <h2 class="title mb-4">Events organised by the Commission</h2>
+				<p> </p>
+			  
+			  <h2 class="title mb-4">Webinar on New Education Policy 2020, through Google meet organized in the State by the Commission:</h2>
+				<ul>
+					<li>Una HP, on 13.10.20, Bilaspur on 14.10.2020, Lahaul and Spiti 16.10.2020, Kullu 17.10.2020, Solan 19.10.2020 and Shimla 20.10.2020 with colorations of SSA and concerned DPOs. About 80 to 100 shikshak/participants joined the meet in each Distt. </li> 
+					<li>Webinars on same subject organized in Disttss viz: Hamirpur on 11.11.2020, Mandi on 17.11.2020, Sirmour and Kangra on 18.11.2020, Kinnaur on 19.11.2020 and Chamba on 20.11.2020, about 80 to 90 teachers and other Participants Joined the Webinar in each District. </li> 
+				</ul>						
+
+              <!--<p><a href="#" class="btn btn-primary rounded-0 px-4">Donate Now</a></p>-->
+            </div>
+          </div>
+		  <!--
+          <div class="col-md-7">
+            <div class="cause shadow-sm">
+              
+                <a href="#" class="cause-link d-block">
+                  <img src="images/img_1.jpg" alt="Image" class="img-fluid">
+                  <div class="custom-progress-wrap">
+                    <span class="caption">80% complete</span>
+                    <div class="custom-progress-inner">
+                      <div class="custom-progress bg-danger" style="width: 80%;"></div>
+                    </div>
+                  </div>
+                </a>
+
+                <div class="px-3 pt-3 border-top-0 border border shadow-sm">
+                  <span class="badge-danger py-1 small px-2 rounded mb-3 d-inline-block">School</span>
+                  <h3 class="mb-4"><a href="#">Alias Odit Ipsam Quas Unde Obcaecati</a></h3>
+                  <div class="border-top border-light border-bottom py-2 d-flex">
+                    <div>Donated</div>
+                    <div class="ml-auto"><strong class="text-primary">$32,919</strong></div>
+                  </div>
+
+                  <div class="py-4">
+                    <div class="d-flex align-items-center">
+                      <img src="images/person_1.jpg" alt="Image" class="rounded-circle mr-3" width="50">
+                      <div class="">James Smith</div>
+                    </div>
+                  </div>
+                </div>
+              
+              </div>
+          </div>
+		  -->
+        </div>
+<!--
+        <div class="row">
+          <div class="col-md-4">
+              <div class="cause shadow-sm">
+              
+                <a href="#" class="cause-link d-block">
+                  <img src="images/img_1.jpg" alt="Image" class="img-fluid">
+                  <div class="custom-progress-wrap">
+                    <span class="caption">80% complete</span>
+                    <div class="custom-progress-inner">
+                      <div class="custom-progress bg-danger" style="width: 80%;"></div>
+                    </div>
+                  </div>
+                </a>
+
+                <div class="px-3 pt-3 border-top-0 border border shadow-sm">
+                  <span class="badge-danger py-1 small px-2 rounded mb-3 d-inline-block">School</span>
+                  <h3 class="mb-4"><a href="#">Alias Odit Ipsam Quas Unde Obcaecati</a></h3>
+                  <div class="border-top border-light border-bottom py-2 d-flex">
+                    <div>Donated</div>
+                    <div class="ml-auto"><strong class="text-primary">$32,919</strong></div>
+                  </div>
+
+                  <div class="py-4">
+                    <div class="d-flex align-items-center">
+                      <img src="images/person_1.jpg" alt="Image" class="rounded-circle mr-3" width="50">
+                      <div class="">James Smith</div>
+                    </div>
+                  </div>
+                </div>
+              
+              </div>
+          </div>
+          <div class="col-md-4">
+            
+            <div class="cause shadow-sm">
+              
+                <a href="#" class="cause-link d-block">
+                  <img src="images/img_2.jpg" alt="Image" class="img-fluid">
+                  <div class="custom-progress-wrap">
+                    <span class="caption">80% complete</span>
+                    <div class="custom-progress-inner">
+                      <div class="custom-progress bg-primary" style="width: 80%;"></div>
+                    </div>
+                  </div>
+                </a>
+
+                <div class="px-3 pt-3 border-top-0 border border shadow-sm">
+                  <span class="badge-primary py-1 small px-2 rounded mb-3 d-inline-block">Health</span>
+                  <h3 class="mb-4"><a href="#">Alias Odit Ipsam Quas Unde Obcaecati</a></h3>
+                  <div class="border-top border-light border-bottom py-2 d-flex">
+                    <div>Donated</div>
+                    <div class="ml-auto"><strong class="text-primary">$32,919</strong></div>
+                  </div>
+
+                  <div class="py-4">
+                    <div class="d-flex align-items-center">
+                      <img src="images/person_1.jpg" alt="Image" class="rounded-circle mr-3" width="50">
+                      <div class="">James Smith</div>
+                    </div>
+                  </div>
+                </div>
+              
+              </div>
+
+          </div>
+          <div class="col-md-4">
+            
+            <div class="cause shadow-sm">
+              
+                <a href="#" class="cause-link d-block">
+                  <img src="images/img_3.jpg" alt="Image" class="img-fluid">
+                  <div class="custom-progress-wrap">
+                    <span class="caption">80% complete</span>
+                    <div class="custom-progress-inner">
+                      <div class="custom-progress bg-warning" style="width: 80%;"></div>
+                    </div>
+                  </div>
+                </a>
+
+                <div class="px-3 pt-3 border-top-0 border border ">
+                  <span class="badge-warning py-1 small px-2 rounded mb-3 d-inline-block">Livelihood</span>
+                  <h3 class="mb-4"><a href="#">Alias Odit Ipsam Quas Unde Obcaecati</a></h3>
+                  <div class="border-top border-light border-bottom py-2 d-flex">
+                    <div>Donated</div>
+                    <div class="ml-auto"><strong class="text-primary">$32,919</strong></div>
+                  </div>
+
+                  <div class="py-4">
+                    <div class="d-flex align-items-center">
+                      <img src="images/person_1.jpg" alt="Image" class="rounded-circle mr-3" width="50">
+                      <div class="">James Smith</div>
+                    </div>
+                  </div>
+                </div>
+              
+              </div>
+
+          </div>
+
+
+          <div class="col-md-4">
+              <div class="cause shadow-sm">
+              
+                <a href="#" class="cause-link d-block">
+                  <img src="images/img_1.jpg" alt="Image" class="img-fluid">
+                  <div class="custom-progress-wrap">
+                    <span class="caption">80% complete</span>
+                    <div class="custom-progress-inner">
+                      <div class="custom-progress bg-danger" style="width: 80%;"></div>
+                    </div>
+                  </div>
+                </a>
+
+                <div class="px-3 pt-3 border-top-0 border border shadow-sm">
+                  <span class="badge-danger py-1 small px-2 rounded mb-3 d-inline-block">School</span>
+                  <h3 class="mb-4"><a href="#">Alias Odit Ipsam Quas Unde Obcaecati</a></h3>
+                  <div class="border-top border-light border-bottom py-2 d-flex">
+                    <div>Donated</div>
+                    <div class="ml-auto"><strong class="text-primary">$32,919</strong></div>
+                  </div>
+
+                  <div class="py-4">
+                    <div class="d-flex align-items-center">
+                      <img src="images/person_1.jpg" alt="Image" class="rounded-circle mr-3" width="50">
+                      <div class="">James Smith</div>
+                    </div>
+                  </div>
+                </div>
+              
+              </div>
+          </div>
+          <div class="col-md-4">
+            
+            <div class="cause shadow-sm">
+              
+                <a href="#" class="cause-link d-block">
+                  <img src="images/img_2.jpg" alt="Image" class="img-fluid">
+                  <div class="custom-progress-wrap">
+                    <span class="caption">80% complete</span>
+                    <div class="custom-progress-inner">
+                      <div class="custom-progress bg-primary" style="width: 80%;"></div>
+                    </div>
+                  </div>
+                </a>
+
+                <div class="px-3 pt-3 border-top-0 border border shadow-sm">
+                  <span class="badge-primary py-1 small px-2 rounded mb-3 d-inline-block">Health</span>
+                  <h3 class="mb-4"><a href="#">Alias Odit Ipsam Quas Unde Obcaecati</a></h3>
+                  <div class="border-top border-light border-bottom py-2 d-flex">
+                    <div>Donated</div>
+                    <div class="ml-auto"><strong class="text-primary">$32,919</strong></div>
+                  </div>
+
+                  <div class="py-4">
+                    <div class="d-flex align-items-center">
+                      <img src="images/person_1.jpg" alt="Image" class="rounded-circle mr-3" width="50">
+                      <div class="">James Smith</div>
+                    </div>
+                  </div>
+                </div>
+              
+              </div>
+
+          </div>
+          <div class="col-md-4">
+            
+            <div class="cause shadow-sm">
+              
+                <a href="#" class="cause-link d-block">
+                  <img src="images/img_3.jpg" alt="Image" class="img-fluid">
+                  <div class="custom-progress-wrap">
+                    <span class="caption">80% complete</span>
+                    <div class="custom-progress-inner">
+                      <div class="custom-progress bg-warning" style="width: 80%;"></div>
+                    </div>
+                  </div>
+                </a>
+
+                <div class="px-3 pt-3 border-top-0 border border ">
+                  <span class="badge-warning py-1 small px-2 rounded mb-3 d-inline-block">Livelihood</span>
+                  <h3 class="mb-4"><a href="#">Alias Odit Ipsam Quas Unde Obcaecati</a></h3>
+                  <div class="border-top border-light border-bottom py-2 d-flex">
+                    <div>Donated</div>
+                    <div class="ml-auto"><strong class="text-primary">$32,919</strong></div>
+                  </div>
+
+                  <div class="py-4">
+                    <div class="d-flex align-items-center">
+                      <img src="images/person_1.jpg" alt="Image" class="rounded-circle mr-3" width="50">
+                      <div class="">James Smith</div>
+                    </div>
+                  </div>
+                </div>
+              
+              </div>
+
+          </div>
+
+          <div class="col-12">
+            <span class="p-3">1</span>
+            <a href="#" class="p-3">2</a>
+            <a href="#" class="p-3">3</a>
+            <a href="#" class="p-3">4</a>
+          </div>
+
+
+
+        </div>
+      </div>
+    </div>
+-->
+    
+
+
+    
+<!-- Footer Section Start --> 
+<?php
+	include ("_partials/footer.php");
+?>
+<!-- Footer Section End -->
+
+  </body>
+  
+<script nonce='S51U26wMQz' type="text/javascript"> 
+    $(document).ready(function() {
+       $('#readmorelnk').click(function() {
+           $('#readmorediv').show();
+           //return false;
+       });        
+   });
+</script>
+
+</html>
